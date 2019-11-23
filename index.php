8ad267ff8cbe6addafbac834bbfff6b54127b1db
kylin-1q84 <shadekj@naver.com>
2019-11-23 21:26:53 +0000

    Update pull button

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
+<?php
+	if (isset($_POST['button']))
+	{
+		exec('pull.sh')
+	}
+?>
 <html>
 <h1> Welcome DB world! Sibal </h1>
 Test
+<form method="post">
+	<button name="button">pull</button>
+</form>
+
 </html>
