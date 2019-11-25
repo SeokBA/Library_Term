0e1c531dbb109ed5523d2259454464520d875218
SeokBA <cpswo123@gmail.com>
2019-11-26 02:51:12 +0900

    update login

--- library/login/loadPW.php
+++ library/login/loadPW.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 $id = $_POST["id"];
-$sql = "select password from User_Account where id = '{id}';";
+$sql = "select password from User_Account where id = '{$id}';";
 $result = mysqli_query($_SESSION['conn'], $sql);
 $row = mysqli_fetch_array($result);
 $dataPW = $row['password'];
