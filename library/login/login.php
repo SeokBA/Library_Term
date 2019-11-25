44cd9f311edb934ab25cdb011d1a7c1c0ac4c111
SeokBA <cpswo123@gmail.com>
2019-11-26 03:11:16 +0900

    update login

--- library/login/login.php
+++ library/login/login.php
@@ -1,7 +1,7 @@
 <?php
 $sql = "select password from User_Account where id = 'admin';";
 $result = mysqli_query(mysqli_connect('112.166.141.161', 'root', 'kylin1q2w3e4r', 'LB_DB'), $sql);
-echo mysqli_fetch_array($result);
+echo $result;
 ?>
 
 <!DOCTYPE html>
