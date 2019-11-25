00354cd47d206d402422598c837ed36dc70da193
SeokBA <cpswo123@gmail.com>
2019-11-25 20:39:58 +0900

    update login

--- index.php
+++ index.php
@@ -1,14 +1,2 @@
 <?php
-	if (isset($_POST['button']))
-	{
-		exec('pull.sh')
-	}
-?>
-<html>
-<h1> Welcome DB world! Sibal </h1>
-Test3
-<form method="post">
-	<button name="button">pull</button>
-</form>
-
-</html>
+Header("Location: /login/login.html");
--- library/index.php
+++
@@ -1,2 +0,0 @@
-<?php
-Header("Location: /login/login.html");
