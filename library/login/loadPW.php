017c4ef4b2b25837387b394d0c1ed2ead2dbf543
SeokBA <cpswo123@gmail.com>
2019-11-26 03:02:07 +0900

    update login

--- library/login/loadPW.php
+++ library/login/loadPW.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$id = $_POST["id"];
+$id = $_GET["id"];
 $sql = "select password from User_Account where id = '{$id}';";
 $result = mysqli_query($_SESSION['conn'], $sql);
 $row = mysqli_fetch_array($result);
--- library/login/signUp.php
+++ library/login/signUp.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
-$id = $_POST["id"];
-$pw = $_POST["pw"];
-$name = $_POST["name"];
-$email = $_POST["email"];
-$phone = $_POST["phone"];
-$classification = $_POST["classification"];
+$id = $_GET["id"];
+$pw = $_GET["pw"];
+$name = $_GET["name"];
+$email = $_GET["email"];
+$phone = $_GET["phone"];
+$classification = $_GET["classification"];
 $sql = "INSERT INTO 'LB_DB'.'User_Account' ('id', 'password', 'name', 'email', 'phone', 'classification') VALUES ('{$id}', '{$pw}', '{$name}', '{$email}', '{$phone}', '{$classification}');";
 $result = mysqli_query($_SESSION['conn'], $sql);
 echo $result;
