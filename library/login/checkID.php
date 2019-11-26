b137a687b073ca01da32e5e8e406aac6fc97ac60
SeokBA <cpswo123@gmail.com>
2019-11-26 10:11:46 +0900

    update login

--- library/login/checkID.php
+++ library/login/checkID.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-$id = $_POST["id"];
-$sql = "select id from User_Account where id = {id};";
+$id = $_REQUEST["id"];
+$sql = "SELECT id FROM User_Account WHERE id = {id};";
 $result = mysqli_query($_SESSION['conn'], $sql);
 $row = mysqli_fetch_array($result);
 echo ($row == null);
--- library/login/signUp.php
+++ library/login/signUp.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
-$id = $_GET["id"];
-$pw = $_GET["pw"];
-$name = $_GET["name"];
-$email = $_GET["email"];
-$phone = $_GET["phone"];
+$id = $_REQUEST["id"];
+$pw = $_REQUEST["pw"];
+$name = $_REQUEST["name"];
+$email = $_REQUEST["email"];
+$phone = $_REQUEST["phone"];
 $classification = $_GET["classification"];
-$sql = "INSERT INTO 'LB_DB'.'User_Account' ('id', 'password', 'name', 'email', 'phone', 'classification') VALUES ('{$id}', '{$pw}', '{$name}', '{$email}', '{$phone}', '{$classification}');";
+$sql = "INSERT INTO 'LB_DB'.'User_Account' ('id', 'password', 'name', 'email', 'phone', 'classification') VALUES ('$id', '$pw', '$name', '$email', '$phone', '$classification');";
 $result = mysqli_query($_SESSION['conn'], $sql);
 echo $result;
