55aa846cec3ee09059d1b9d64bdaeb7d9fae4f9e
SeokBA <cpswo123@gmail.com>
2019-11-26 13:04:46 +0900

    update login

--- library/login/signUp.php
+++ library/login/signUp.php
@@ -9,8 +9,8 @@ $classification = $_GET["classificationSignUpBox"];
 $conn = mysqli_connect('112.166.141.161', 'root', 'kylin1q2w3e4r', 'LB_DB');
 $sql = "SELECT * FROM User_Account WHERE id = '{$id}';";
 $result = mysqli_query($conn, $sql);
-$row = mysqli_fetch_array($result)
-echo ("<script> alert({$result}); alert({$row}); </script>");
+$row = mysqli_fetch_array($result);
+echo ("<script> alert('hi'); alert($result); alert({$row}); </script>");
 
 //$sql = "INSERT INTO 'LB_DB'.'User_Account' ('id', 'password', 'name', 'email', 'phone', 'classification') VALUES ('{$id}', '{$pw}', '{$name}', '{$email}', '{$phone}', '{$classification}');";
 //$result = mysqli_query($_SESSION['conn'], $sql);
