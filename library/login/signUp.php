6d9d7b614aa89f99709c6b38871582dba2af8df9
SeokBA <cpswo123@gmail.com>
2019-11-26 12:57:12 +0900

    update login

--- library/login/signUp.php
+++ library/login/signUp.php
@@ -13,6 +13,6 @@ echo $result;
 if ($result != null)
     echo ("<script> alert('Duplicate ID') </script>");
 
-//$sql = "INSERT INTO 'LB_DB'.'User_Account' ('id', 'password', 'name', 'email', 'phone', 'classification') VALUES ('$id', '$pw', '$name', '$email', '$phone', '$classification');";
+//$sql = "INSERT INTO 'LB_DB'.'User_Account' ('id', 'password', 'name', 'email', 'phone', 'classification') VALUES ('{$id}', '{$pw}', '{$name}', '{$email}', '{$phone}', '{$classification}');";
 //$result = mysqli_query($_SESSION['conn'], $sql);
 //echo ("<script> alert('Done') </script>");
