a8efa8f2c5df38c71d8a6469300e29c949467853
SeokBA <cpswo123@gmail.com>
2019-12-01 10:29:25 +0900

    update adminPage.php

--- library/main/admin/adminPage.php
+++ library/main/admin/adminPage.php
@@ -42,7 +42,7 @@
             $bookStateRow = mysqli_fetch_array($result);
             $rowNum = $result->num_rows;
             for($i = 0; $i < $rowNum; $i++) {
-                echo "<p>hi{i}hi</p>";
+                echo "<p>hi{$i}hi</p>";
                 $isbn = $bookStateRow['ISBN'][$i];
                 $sql = "SELECT * FROM Book_Information WHERE ISBN = '{$isbn}'";
                 $bookInformation = mysqli_query($_SESSION['conn'], $sql);
