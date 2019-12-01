d96784b80d1c5b19af505800d35e37997d565b50
SeokBA <cpswo123@gmail.com>
2019-12-01 10:27:01 +0900

    update adminPage.php

--- library/main/admin/adminPage.php
+++ library/main/admin/adminPage.php
@@ -16,8 +16,11 @@
     $_SESSION['conn'] = mysqli_connect('112.166.141.161', 'root', 'kylin1q2w3e4r', 'LB_DB');
     echo "<p align='right'>id : {$_SESSION['id']} <input type='button' value='정보수정' onclick='clickModal'()> </p>";
     ?>
-    <?php
-    echo "<p>hi{$_SESSION['conn']}hi</p>"
+    <?php$sql = "SELECT * FROM Book_Statement WHERE reservation_chk = 0";
+    $result = mysqli_query($_SESSION['conn'], $sql);
+    $bookStateRow = mysqli_fetch_array($result);
+    $rowNum = $result->num_rows;
+    echo "<p>hi{$rowNum}hi</p>"
     ?>
 </div>
 <div class="sidebar" style="width:13%">
