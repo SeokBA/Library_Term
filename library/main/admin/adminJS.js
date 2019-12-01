2112fa3f99bc7a38731f8356569b7275e12e9915
SeokBA <cpswo123@gmail.com>
2019-12-01 11:38:41 +0900

    update adminPage.php

--- library/main/admin/adminJS.js
+++ library/main/admin/adminJS.js
@@ -2,12 +2,12 @@ let bookList = document.getElementById("bookList");
 let returnbook = document.getElementById("returnBook");
 let Manage = document.getElementById("Manage");
 
-function bookUpdate(name, isbn, author, publisher){
+function bookUpdate(name){
     alert("hello");
     alert(name);
-    alert(isbn);
-    alert(author);
-    alert(publisher);
+    // alert(isbn);
+    // alert(author);
+    // alert(publisher);
 }
 
 function bookRemove(bookId) {
